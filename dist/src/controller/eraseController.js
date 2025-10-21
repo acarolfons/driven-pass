1d53a75e0d1a8d9d4c412fc6a037f8bc058766d0
Ana Carolina <carolina@MacBook-Pro-de-Ana.local>
2025-10-21 17:40:34 -0300

    last commit: testes e tudo ok

--- dist/src/controller/eraseController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.eraseAccount = eraseAccount;
-const eraseService_1 = require("../services/eraseService");
-async function eraseAccount(req, res, next) {
-    const userId = req.user.userId;
-    await (0, eraseService_1.deleteAccount)(userId);
-    res.sendStatus(204);
-}
